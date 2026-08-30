c6a575419029c9324f5c99ee2fb4d970c05310c2
Youssef Shrief  Ebrahim <fofoahrief129@gmail.com>
2026-08-30 22:17:20 +0300

    Delete components directory

--- components/Logo.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import LogoRNE from "./LogoRNE";
-
-// Back-compat wrapper. compact -> smaller size.
-export default function Logo({ compact = false, light = false }) {
-  return <LogoRNE size={compact ? "sm" : "md"} light={light} />;
-}
